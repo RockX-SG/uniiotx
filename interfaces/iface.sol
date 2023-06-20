25a5128946d6798a4c65c4c2343749266bb5686d
Zealy-Rockx <jigao@rockx.com>
2023-06-20 11:40:22 +0800

    add bucketOf function.

--- interfaces/iface.sol
+++ interfaces/iface.sol
@@ -8,6 +8,8 @@ interface IMintableContract is IERC20 {
 // @notice This is the IoTeX system contract interface. It issues an NFT token for each bucket creation.
 // For more information see https://github.com/iotexproject/iip13-contracts/blob/main/src/SystemStaking.sol
 interface ISystemStakingContract {
+    function bucketOf(uint256 _tokenId) external view returns ( uint256 amount_, uint256 duration_, uint256 unlockedAt_, uint256 unstakedAt_, address delegate_);
+
     function stake(uint256 _duration, address _delegate) external payable returns (uint256);
     function stake(uint256 _amount, uint256 _duration, address[] memory _delegates) external payable returns (uint256 firstTokenId_);
     function stake(uint256 _amount, uint256 _duration, address _delegate, uint256 _count) external payable returns (uint256 firstTokenId_);
