8283421f2aad2ee7ced8aff0620f541387392dcb
Zealy-Rockx <jigao@rockx.com>
2023-06-22 06:51:18 +0800

    add extra TIOTXClear methods.

--- contracts/interfaces/IIOTXClear.sol
+++ contracts/interfaces/IIOTXClear.sol
@@ -1,11 +1,10 @@
 pragma solidity ^0.8.9;
 
 interface IIOTXClear {
-    function joinDebt(address claimAddr, uint256 amount) external;
+    function joinDebt(address claimAddr, uint256 amount) public;
     function unstake(uint256[] calldata tokenIds) external;
     function withdraw(uint256[] calldata tokenIds) external;
     function claimRewards(uint256 amount) external;
     function changeDelegate(uint256 tokenId, address delegate) external;
     function setDelegate(address delegate);
-    function payDebt(address account) external payable;
 }
