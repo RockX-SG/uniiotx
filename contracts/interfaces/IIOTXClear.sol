879a6a36624e64e48113cea2df0a8f1211e4669c
Zealy-Rockx <jigao@rockx.com>
2023-07-07 18:06:42 +0800

    update IIOTXClear and IIOTXStake interfaces.

--- contracts/interfaces/IIOTXClear.sol
+++ contracts/interfaces/IIOTXClear.sol
@@ -1,10 +1,12 @@
 pragma solidity ^0.8.9;
 
 interface IIOTXClear {
+    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4);
+    function updateDelegates(uint256[] tokenIds, address delegate) external;
+    function getReward(address acount) external returns (uint256);
     function joinDebt(address claimAddr, uint256 amount) public;
     function unstake(uint256[] calldata tokenIds) external;
     function withdraw(uint256[] calldata tokenIds) external;
+    function updateReward(address acount) external returns (uint256);
     function claimRewards(uint256 amount) external;
-    function changeDelegate(uint256 tokenId, address delegate) external;
-    function setDelegate(address delegate);
 }
--- contracts/interfaces/IIOTXStake.sol
+++ contracts/interfaces/IIOTXStake.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.8.9;
 
 interface IIOTXStake {
-    // Todo: Add proper functions based on IOTXStake contract.
+    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4);
+    function exchangeRatio() external returns (uint256 ratio);
+    function currentReserve() public view returns(uint256);
+    function getRedeemedTokenIds(uint256 i, uint256 j) external view returns (uint256[] memory tokenIds);
+    function setGlobalDelegate(address delegate) external;
+    function updateDelegates(uint256[] tokenIds, address delegate) external;
+    function deposit(uint256 minToMint, uint256 deadline) external payable returns (uint256 minted);
+    function stake() external;
+    function redeem(uint256 iotxsToRedeem, uint256 maxToBurn, uint256 deadline) external returns (uint256 burned);
+    function updateReward() external;
+    function withdrawManagerFee(uint256 amount, address recipient) external;
 }
