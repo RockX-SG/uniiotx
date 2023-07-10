cb47b1dfcdb37417b1d78e35f7682ac677fd4783
Zealy-Rockx <jigao@rockx.com>
2023-07-10 16:07:27 +0800

    code cleanup

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.9;
 
 // Roles
 bytes32 constant ROLE_PAUSE = keccak256("ROLE_PAUSE");
-bytes32 constant ROLE_MINT = keccak256("ROLE_MINT"); // Todo: Maybe remove it
-bytes32 constant ROLE_STAKE = keccak256("ROLE_STAKE"); // Todo: Maybe remove it
+bytes32 constant ROLE_MINT = keccak256("ROLE_MINT");
+bytes32 constant ROLE_STAKE = keccak256("ROLE_STAKE");
 bytes32 constant ROLE_FEE_MANAGER = keccak256("ROLE_FEE_MANAGER");
 bytes32 constant ROLE_ORACLE = keccak256("ROLE_ORACLE");
 
