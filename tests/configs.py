554de035c7cca3b8325009d5fdadf61f4bc08ad0
Zealy-Rockx <jigao@rockx.com>
2023-07-25 17:38:42 +0800

    tests: check permission

--- tests/configs.py
+++ tests/configs.py
@@ -1,5 +1,19 @@
 import pytest
 from brownie import accounts
+from web3 import Web3
+
+# Roles
+@pytest.fixture
+def roles():
+    # Update ROLE_ORACLE provided by backend developer
+    w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+    role_pause = w3.keccak(text='ROLE_PAUSE')  # index = 0
+    role_mint = w3.keccak(text='ROLE_MINT')  # index = 1
+    role_stake = w3.keccak(text='ROLE_STAKE')  # index = 2
+    role_fee_manager = w3.keccak(text='ROLE_FEE_MANAGER')  # index = 3
+    role_oracle = w3.keccak(text='ROLE_ORACLE')  # index = 4
+    role_default_admin = w3.toBytes(hexstr="0x00")  # index = 5
+    return [role_pause, role_mint, role_stake, role_fee_manager, role_oracle, role_default_admin]
 
 # Predefined Accounts
 @pytest.fixture
--- tests/test_init_status.py
+++ tests/test_init_status.py
@@ -1,11 +1,32 @@
 from configs import *
 from contracts import *
 
-def test_init_status(delegates, start_amount, common_ratio, sequence_length, stake_amounts, stake_duration, manager_fee_shares, contracts):
+def test_init_status(roles, owner, admin, delegates, oracle, start_amount, common_ratio, sequence_length, stake_amounts, stake_duration, manager_fee_shares, contracts):
     system_staking, uni_iotx, iotx_clear, iotx_stake = contracts[0], contracts[1], contracts[2], contracts[3]
+
+    # Check permissions
+    assert system_staking.owner() == owner.address
+
+    assert uni_iotx.hasRole(roles[5], admin.address)  # ROLE_DEFAULT_ADMIN
+    assert uni_iotx.hasRole(roles[0], iotx_stake.address)  # ROLE_PAUSE
+    assert uni_iotx.hasRole(roles[1], iotx_stake.address)  # ROLE_MING
+
+    assert iotx_clear.hasRole(roles[5], admin.address)  # ROLE_DEFAULT_ADMIN
+    assert iotx_clear.hasRole(roles[2], iotx_stake.address)  # ROLE_STAKE
+    assert iotx_clear.hasRole(roles[4], oracle.address)  # ROLE_ORACLE
+
+    assert iotx_stake.hasRole(roles[5], admin.address)  # ROLE_DEFAULT_ADMIN
+    assert iotx_stake.hasRole(roles[3], admin.address)  # ROLE_FEE_MANAGER
+    assert iotx_stake.hasRole(roles[0], admin.address)  # ROLE_PAUSE
+    assert iotx_stake.hasRole(roles[4], oracle.address)  # ROLE_ORACLE
+
+    # Check initial status variables
     assert system_staking.UNSTAKE_FREEZE_BLOCKS() == 1
+
     assert uni_iotx.symbol() == "uniIOTX"
+
     assert iotx_clear.MULTIPLIER() == 1000000000000000000
+
     assert iotx_stake.defaultExchangeRatio() == 1
     assert iotx_stake.MULTIPLIER() == 1000000000000000000
     assert iotx_stake.globalDelegate() == delegates[0].address
