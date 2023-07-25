777cb2fee4ddfd160d308b37ac36acf9888d7ca0
Zealy-Rockx <jigao@rockx.com>
2023-07-25 08:16:34 +0800

    scripts/deploy: update deployment

--- scripts/deploy/testnet.py
+++ scripts/deploy/testnet.py
@@ -42,22 +42,23 @@ def main():
     delegate = "0xac82586b613d10a33df00835aC6DAd8541952334"  # io14jp9s6mp85g2x00spq66cmdds4qe2ge5r0p72d
 
     # Deploy contracts
-    uni_iotx = UniIOTX.deploy({'from': deployer})  # https://testnet.iotexscan.io/address/0x472AC984B40D1e73Ac6ad3642bfbBE3be3D778E1#transactions
-    uni_iotx_proxy = TransparentUpgradeableProxy.deploy(uni_iotx, deployer, b'', {'from': deployer})  # https://testnet.iotexscan.io/address/0x73fA15Aa90F4F54F093bc7ECDF3906633D2eB194#transactions
+    gas_limit = '6721975'
+    uni_iotx = UniIOTX.deploy({'from': deployer, 'gas_limit': gas_limit})  # https://testnet.iotexscan.io/address/0xa223567B74581F1B995b346e36e26C291d1cC1B4#transactions
+    uni_iotx_proxy = TransparentUpgradeableProxy.deploy(uni_iotx, deployer, b'', {'from': deployer, 'gas_limit': gas_limit})  # https://testnet.iotexscan.io/address/0x956a03ecEb344eA15A6CbE8949088992fAD88628#transactions
 
-    iotx_clear = IOTXClear.deploy({'from': deployer})  # https://testnet.iotexscan.io/address/0x54aE7559E11E4Ed483C9b5557E9aF349d1d29b1b#transactions
-    iotx_clear_proxy = TransparentUpgradeableProxy.deploy(iotx_clear, deployer, b'', {'from': deployer})  # https://testnet.iotexscan.io/address/0x703832231D3AbBDef4833cCC510ac7E06abDE5b7#transactions
+    iotx_clear = IOTXClear.deploy({'from': deployer, 'gas_limit': gas_limit})  # https://testnet.iotexscan.io/address/0x7c193769f46D2B32819eb76E7c9Aeb580260A668#transactions
+    iotx_clear_proxy = TransparentUpgradeableProxy.deploy(iotx_clear, deployer, b'', {'from': deployer, 'gas_limit': gas_limit})  # https://testnet.iotexscan.io/address/0x4DC32Ad7BffAF50434b12195D3b59CD66601335D#transactions
 
-    iotx_stake = IOTXStake.deploy({'from': deployer})  # https://testnet.iotexscan.io/address/0x6141572c66D5ff818678576a5790226047DF0bc0#transactions
-    iotx_stake_proxy = TransparentUpgradeableProxy.deploy(iotx_stake, deployer, b'', {'from': deployer})  # https://testnet.iotexscan.io/address/0x22Ca5Bf95e7d8514FfE0605d3fFb1993660F3280#transactions
+    iotx_stake = IOTXStake.deploy({'from': deployer, 'gas_limit': gas_limit})  # https://testnet.iotexscan.io/address/0x171977bf57e93C9be280569F4853325719130C22#transactions
+    iotx_stake_proxy = TransparentUpgradeableProxy.deploy(iotx_stake, deployer, b'', {'from': deployer, 'gas_limit': gas_limit})  # https://testnet.iotexscan.io/address/0xa479659F378d54168CD7859f5025133382EdB3C5#transactions
 
     uni_iotx_transparent = Contract.from_abi("UniIOTX", uni_iotx_proxy.address, UniIOTX.abi)
     iotx_clear_transparent = Contract.from_abi("IOTXClear", iotx_clear_proxy.address, IOTXClear.abi)
     iotx_stake_transparent = Contract.from_abi("IOTXStake", iotx_stake_proxy.address, IOTXStake.abi)
 
-    print("Deployed UniIOTX address:", uni_iotx_transparent)  # https://testnet.iotexscan.io/address/0x73fA15Aa90F4F54F093bc7ECDF3906633D2eB194#transactions
-    print("Deployed IOTXClear address:", iotx_clear_transparent)  # https://testnet.iotexscan.io/address/0x703832231D3AbBDef4833cCC510ac7E06abDE5b7#transactions
-    print("Deployed IOTXStake address:", iotx_stake_transparent)  # https://testnet.iotexscan.io/address/0x22Ca5Bf95e7d8514FfE0605d3fFb1993660F3280#transactions
+    print("Deployed UniIOTX address:", uni_iotx_transparent)  # https://testnet.iotexscan.io/address/0x956a03ecEb344eA15A6CbE8949088992fAD88628#transactions
+    print("Deployed IOTXClear address:", iotx_clear_transparent)  # https://testnet.iotexscan.io/address/0x4DC32Ad7BffAF50434b12195D3b59CD66601335D#transactions
+    print("Deployed IOTXStake address:", iotx_stake_transparent)  # https://testnet.iotexscan.io/address/0xa479659F378d54168CD7859f5025133382EdB3C5#transactions
 
     uni_iotx_transparent.initialize(iotx_stake_transparent, {'from': admin})
     iotx_clear_transparent.initialize(system_staking, iotx_stake_transparent, oracle, {'from': admin})
