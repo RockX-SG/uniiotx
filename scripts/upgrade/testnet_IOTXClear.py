74fce6007a4159cf37e5cb72951c0b7e195f92aa
Zealy-Rockx <jigao@rockx.com>
2023-08-02 14:15:43 +0800

    scripts/upgrade: upgrade IOTXClear contract.

--- scripts/upgrade/testnet_IOTXClear.py
+++ scripts/upgrade/testnet_IOTXClear.py
@@ -14,15 +14,28 @@ def main():
     deps = project.load(Path.home() / ".brownie" / "packages" / config["dependencies"][0])
     TransparentUpgradeableProxy = deps.TransparentUpgradeableProxy
 
+    # SystemStaking contract address
+    # https://testnet.iotexscan.io/address/0x52ab0fe2c3a94644de0888a3ba9ea1443672e61f#transactions
+    system_staking = "0x52ab0fe2c3a94644de0888a3ba9ea1443672e61f"
+
+    # IOTXStake contract address
+    # https://testnet.iotexscan.io/address/0xa479659F378d54168CD7859f5025133382EdB3C5#transactions
+    iotx_stake_transparent = "0xa479659F378d54168CD7859f5025133382EdB3C5"
+
+    # Load accounts
     deployer = accounts.load("IoTeXDeployer")
+    oracle = accounts.load("IoTeXOracle")
+    admin = accounts.load("IoTeXAdmin")
+
     gas_limit = '6721975'
 
     iotx_clear_proxy_addr = "0x4DC32Ad7BffAF50434b12195D3b59CD66601335D"
     iotx_clear_proxy = TransparentUpgradeableProxy.at(iotx_clear_proxy_addr)
 
     iotx_clear_upgraded = IOTXClear.deploy({'from': deployer, 'gas_limit': gas_limit})
+    iotx_clear_upgraded.initialize(system_staking, iotx_stake_transparent, oracle, {'from': admin, 'gas_limit': gas_limit})
     iotx_clear_proxy.upgradeTo(iotx_clear_upgraded, {'from': deployer, 'gas_limit': gas_limit})
 
-    print("Upgraded IOTXClear address:", iotx_clear_upgraded)  # https://testnet.iotexscan.io/address/0x883Afd0Cb215B36BA2cF98A5542Be63e8b6e9114#transactions
+    print("Upgraded IOTXClear address:", iotx_clear_upgraded)  # https://testnet.iotexscan.io/address/0x990cEAE83B6149A32dEd2db9ef70a21a57bfEf00#transactions
 
 
