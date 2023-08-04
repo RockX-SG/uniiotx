b976a3e77068598d82035ada31aa5abfc381336b
Zealy-Rockx <jigao@rockx.com>
2023-08-04 16:22:06 +0800

    tests: settle debts in installments.

--- tests/test_payDebts.py
+++ tests/test_payDebts.py
@@ -10,41 +10,68 @@ def test_payDebts(w3, contracts, users, delegates, oracle, admin, stake_amounts)
     # ---Happy path testing---
 
     # The user's reward should be updated prior to debt payment.
+    # A substantial debt can be paid off in several installments
+    # until it is fully settled and its corresponding entry is removed.
     # Following the debt payment, both the 'accountedBalance' and the user's principal record should be adjusted.
     # The withdrawal of NFT during debt payment should not compromise the accuracy of reward recording.
+
+    # Users deposit and redeem assets
     deadline = w3.eth.get_block('latest').timestamp+60
     amt = iotx_stake.redeemAmountBase()
     cnt = 10
     amt_total = amt * cnt
     for i in range(0, cnt):
         iotx_stake.deposit(amt, deadline, {'from': users[0], 'value': amt, 'allow_revert': True})
+
     uni_iotx.approve(iotx_stake, amt_total, {'from': users[0], 'allow_revert': True})
     iotx_stake.redeem(amt_total, amt_total, deadline, {'from': users[0], 'allow_revert': True})
-    token_ids = iotx_stake.getRedeemedTokenIds(0, cnt)
+
+    # Mock reward
     mock_reward_incr1 = 1000
     delegates[0].transfer(iotx_clear, mock_reward_incr1)
-    iotx_clear.unstake(token_ids, {'from': oracle, 'allow_revert': True})
-    balance_user01 = users[0].balance()
+
+    # Oracle makes the first debt payment
+    token_ids = iotx_stake.getRedeemedTokenIds(0, cnt)
+    iotx_clear.unstake(token_ids[0:5], {'from': oracle, 'allow_revert': True})
     reward_rate1 = mock_reward_incr1 * 1e18 / amt_total
     reward_user01 = (reward_rate1 - 0) * amt_total / 1e18
-    tx = iotx_clear.payDebts(token_ids, {'from': oracle, 'allow_revert': True})
-    user_info = iotx_clear.userInfos(users[0])
-    debt = iotx_clear.iotxDebts(1)
+    tx = iotx_clear.payDebts(token_ids[0:5], {'from': oracle, 'allow_revert': True})
     assert "Withdrawal" in tx.events
     assert "DebtPaid" in tx.events
-    assert users[0].balance() == balance_user01
+    assert iotx_clear.headIndex() == 0
+    assert iotx_clear.totalDebts() == amt_total/2
+    assert iotx_clear.accountedBalance() == mock_reward_incr1 + amt_total/2
+    assert iotx_clear.balance() == iotx_clear.accountedBalance()
+
+    user_info = iotx_clear.userInfos(users[0])
+    debt_item1 = iotx_clear.iotxDebts(1)
+    assert user_info[0] == amt_total/2
+    assert user_info[1] == amt_total/2
+    assert user_info[2] == reward_user01
+    assert user_info[3] == reward_rate1
+    assert iotx_clear.rewardRate() == reward_rate1
+    assert debt_item1[0] == users[0]
+    assert debt_item1[1] == amt_total/2
+
+    # Oracle makes the second payment
+    iotx_clear.unstake(token_ids[5:cnt], {'from': oracle, 'allow_revert': True})
+    tx = iotx_clear.payDebts(token_ids[5:cnt], {'from': oracle, 'allow_revert': True})
+    assert "Withdrawal" in tx.events
+    assert "DebtPaid" in tx.events
     assert iotx_clear.headIndex() == 1
-    assert iotx_clear.rearIndex() - iotx_clear.headIndex() == 0
     assert iotx_clear.totalDebts() == 0
     assert iotx_clear.accountedBalance() == mock_reward_incr1 + amt_total
     assert iotx_clear.balance() == iotx_clear.accountedBalance()
+
+    user_info = iotx_clear.userInfos(users[0])
+    debt_item1 = iotx_clear.iotxDebts(1)
     assert user_info[0] == 0
     assert user_info[1] == amt_total
     assert user_info[2] == reward_user01
     assert user_info[3] == reward_rate1
     assert iotx_clear.rewardRate() == reward_rate1
-    assert debt[0] == "0x0000000000000000000000000000000000000000"
-    assert debt[1] == 0
+    assert debt_item1[0] == "0x0000000000000000000000000000000000000000"
+    assert debt_item1[1] == 0
 
     # ---Revert path testing---
 
