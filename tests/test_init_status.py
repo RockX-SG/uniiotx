ab094d0acf6b0af801174e121bd0760e518402db
Zealy-Rockx <jigao@rockx.com>
2023-08-21 10:02:08 +0800

    tests: test reinitialization revert.

--- tests/test_init_status.py
+++ tests/test_init_status.py
@@ -1,5 +1,10 @@
+import brownie
+
+
 def test_init_status(fn_isolation, roles, owner, admin, delegates, oracles, start_amount, common_ratio, sequence_length, stake_amounts, stake_duration, manager_fee_shares, contracts):
     system_staking, uni_iotx, iotx_clear, iotx_staking = contracts[0], contracts[1], contracts[2], contracts[3]
+
+    # ---Happy path testing---
 
     # Check permissions
     assert system_staking.owner() == owner.address
@@ -33,3 +38,23 @@ def test_init_status(fn_isolation, roles, owner, admin, delegates, oracles, star
     assert iotx_staking.getRedeemAmountBase() == stake_amounts[2]
     assert iotx_staking.getStakeDuration() == stake_duration
     assert iotx_staking.getManagerFeeShares() == manager_fee_shares
+
+    # ---Revert path testing---
+
+    # Reinitialization is not allowed
+    with brownie .reverts("Initializable: contract is already initialized"):
+        uni_iotx.initialize(iotx_staking, {'from': admin})
+    with brownie .reverts("Initializable: contract is already initialized"):
+        iotx_staking.initialize(
+            system_staking,
+            uni_iotx,
+            iotx_clear,
+            oracles[0],
+            start_amount,
+            common_ratio,
+            sequence_length,
+            stake_duration,
+            {'from': admin}
+        )
+    with brownie .reverts("Initializable: contract is already initialized"):
+        iotx_clear.initialize(system_staking, iotx_staking, oracles[0], {'from': admin})
