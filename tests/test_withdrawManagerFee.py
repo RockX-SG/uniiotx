95563b24e9bc7eb82e09acefe1509a00600814d0
Zealy-Rockx <jigao@rockx.com>
2023-09-05 16:14:50 +0800

    tests: update test_withdrawManagerFee.py

--- tests/test_withdrawManagerFee.py
+++ tests/test_withdrawManagerFee.py
@@ -7,21 +7,27 @@ def test_withdrawManagerFee(fn_isolation, contracts, users, delegates, admin, or
     # ---Happy path testing---
 
     # An appropriate amount of uniIOTX should be minted, and the value of totalPending should be adjusted accordingly.
-    # The value transferred here will be considered as rewards from the delegate.
+    assert iotx_staking.exchangeRatio() == 1e18
     amt = iotx_staking.getRedeemAmountBase()
     iotx_staking.deposit(deadline, {'from': users[0], 'value': amt, 'allow_revert': True})
+    assert iotx_staking.getPendingReward() == 0
     amt_reward = 200
     delegates[0].transfer(iotx_staking, amt_reward)
-    iotx_staking.updateReward({"from": oracles[0]})
     manager_reward = iotx_staking.getManagerFeeShares() * amt_reward / 1000
     user_reward = amt_reward - manager_reward
+    assert iotx_staking.getPendingReward() == amt_reward
+    assert iotx_staking.exchangeRatio() == 1018000000000000000
     assert manager_reward == 20
     to_min = manager_reward / 2 * 1e18 / iotx_staking.exchangeRatio()
     tx = iotx_staking.withdrawManagerFee(manager_reward / 2, admin, {"from": admin})
     assert "ManagerFeeWithdrawed" in tx.events
+    assert iotx_staking.getPendingReward() == 0
     assert iotx_staking.getManagerReward() == manager_reward / 2
     assert iotx_staking.getTotalPending() == user_reward + manager_reward / 2
     assert uni_iotx.balanceOf(admin) == to_min
+    assert iotx_staking.currentReserve() == 10190
+    assert uni_iotx.totalSupply() == 10009
+    assert iotx_staking.exchangeRatio() == 1018083724647816964
 
     # ---Revert path testing---
 
