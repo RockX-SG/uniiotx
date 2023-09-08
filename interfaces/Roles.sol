8b950a7f6c0db26709c46c93d9e975a757cbf152
Zealy-Rockx <jigao@rockx.com>
2023-09-08 09:57:57 +0800

    update copyright notice.

--- interfaces/Roles.sol
+++ interfaces/Roles.sol
@@ -1,7 +1,6 @@
 /*
  * ==================================================================
  * Copyright (C) 2023 Altstake Technology Pte. Ltd. (RockX)
- * CAUTION: THESE CODES HAVE NOT BEEN AUDITED
  * This code is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 3 of the License,
